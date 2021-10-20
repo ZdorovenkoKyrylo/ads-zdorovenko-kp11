ed27b8a29d5e7e570b55cda006de7ba57f6d34b6
ZdorovenkoKyrylo <92879521+ZdorovenkoKyrylo@users.noreply.github.com>
2021-10-20 20:12:36 +0300

    Лабораторна робота1 завершена

---
+++ Лабораторна1/Program1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+namespace Lab_01_ADS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, z, a, b;
+            Console.Write("x: "); x = double.Parse(Console.ReadLine());
+            Console.Write("y: "); y = double.Parse(Console.ReadLine());
+            Console.Write("z: "); z = double.Parse(Console.ReadLine());
+            if (x == -z)
+            {
+                Console.WriteLine("Error");
+            }
+            else if (x == y)
+            {
+                Console.WriteLine("Error");
+            }
+            else if (Math.Log(Math.Abs(y - x)) == -2)
+            {
+                Console.WriteLine("Error");
+            }
+            else
+            {
+                a = Math.Log10((Math.Abs(x + z)) / (1.0 + Math.Log(Math.Abs(y - x)) / 2) + (2 * y));
+                Console.WriteLine("a: " + a.ToString());
+                if (a == 0)
+                {
+                    Console.WriteLine("Error");
+                }
+                else if (a <= -z)
+                {
+                    Console.WriteLine("Error");
+                }
+                else if (x == 0)
+                {
+                    if (a > 0)
+                    {
+                        Console.WriteLine("Error");
+                    }
+                }
+                else if (x < 0)
+                {
+                    if ((1 / a) % 2 == 0)
+                    {
+                        Console.WriteLine("Error");
+                    }
+                }
+                b = Math.Log(a + z) / Math.Pow(a, 2) + Math.Pow(x, -a);
+                Console.WriteLine("b: " + b.ToString());
+            }
+        }
+    }
+}
---
+++ Лабораторна1/Program2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ADS_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c;
+            Console.Write("c:"); c= Int32.Parse(Console.ReadLine());
+            c = (c * 100 - 100);
+
+            int d = 13;
+            int res = 0;
+            double res1;
+            int count = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                c += 1;
+                for (int m = 1; m <= 12; m++)
+                {
+                    res1 = 2.6 * m - 0.2;
+                    res = (Convert.ToInt16(res1) + d + c % 100 + (c % 100) / 4 + (c / 100) / 4 - 2 * c / 100) % 7;
+                    if (res == 5)
+                        count++;
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
